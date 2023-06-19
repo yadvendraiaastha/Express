17030442eab6bc39b818e34f7f9648e49300452c
yadvendraiaastha <yadvendra.yadav@iaastha.com>
2023-06-19 18:20:43 +0530

    update of 19JUne 2023

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
-//1111111111111111
-//bby default request take by server and send response
+// //1111111111111111
+// //bby default request take by server and send response
 
 
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-app.get('/',(req,res) => {
-    res.send("hello home page");
-});
+// app.get('/',(req,res) => {
+//     res.send("hello home page");
+// });
 
-app.get('/example',(req,res) => {
-    res.send("this is example page");
-});
+// app.get('/example',(req,res) => {
+//     res.send("this is example page");
+// });
 
-app.get('/about',(req,res) => {
-    res.send("this is about page");
-});
+// app.get('/about',(req,res) => {
+//     res.send("this is about page");
+// });
 
-app.get('/help',(req,res) => {
-    res.send("this is help page");
+// app.get('/help',(req,res) => {
+//     res.send("this is help page");
 
-});
+// });
 
-app.listen(3000);
+// app.listen(3000);
 
-//222222222222222222222222222222222
-//original req send by client 
+// //222222222222222222222222222222222
+// //original req send by client 
 
-///////////////////////////////
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
+// ///////////////////////////////
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const morgan = require('morgan');
 
-const app = express();
-dotenv.config({path:"config.env"})
-const PORT = process.env.PORT || 8080
+// const app = express();
+// dotenv.config({path:"config.env"})
+// const PORT = process.env.PORT || 8080
 
-app.use(morgan('tiny'));
+// app.use(morgan('tiny'));
 
-app.get('/',(req,res) => {
-    res.send("Task Manager");
-});
-app.listen(PORT,()=> {console.log('Server is running on http://localhost:${PORT}')});
+// app.get('/',(req,res) => {
+//     res.send("Task Manager");
+// });
+// app.listen(PORT,()=> {console.log('Server is running on http://localhost:${PORT}')});
 
 
+process.env.MONGO_URL
+process.env.PORT || 3200
--- index.js
+++ index.js
@@ -28,20 +28,64 @@
 
 // app.listen(2500);
 
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-const userRouter = require("./src/routes/userRoutes");
-//const noteRouter = require("./routes/noteRoutes");
+// const userRouter = require("./src/routes/userRoutes");
+// //const noteRouter = require("./routes/noteRoutes");
 
-app.set("/users",userRouter);
-app.set("/note",noteRoute);
+// app.set("/users",userRouter);
+// app.set("/note",noteRoute);
 
-app.get("/",(req,res) => {
-    res.send('hello');
-});
+// app.get("/",(req,res) => {
+//     res.send('hello');
+// });
 
-app.listen(5300,()=>{
-    console.log("server started on the port number.5300");    
-});
+// app.listen(5300,()=>{
+//     console.log("server started on the port number.5300");    
+// });
 
+const Category = require("../models/category");
+
+const addcategory = async (req, res) => {
+
+
+    const { category } = req.body;
+
+    const Newcategory = new Category({
+        category: category
+    });
+
+
+    try {
+
+        const cat_data = Category.findOne();
+        
+            if(cat_data.length > 0)
+            {
+                let checking = false;
+                for(let i=0;i<cat_data.length;i++)
+                {
+                    if(cat_data[i]['Newcategory'].toLowerCase()===)
+                    {
+
+                    }
+                }
+
+            }else
+            {
+                await Newcategory.save();
+                res.status(201).json({Newcategory});
+
+            }
+        }
+
+          catch (error) {
+        // console.log(error)
+        res.status(500).json({ message: "category note created" });
+
+    }
+
+}
+
+module.exports = { addcategory };
